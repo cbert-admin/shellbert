8b619c8e5ca4ea07c12ddf513ee12e2b957c35e0
cbert <admin@cbert.xyz>
2023-04-21 01:54:14 -0500

    Updated Aliases, added repo pull to login

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -1,2 +1,11 @@
 #!/bin/bash
 alias cl="clear"
+
+# Shorten commands
+alias bat="batcat"
+
+# git related
+alias pull="git pull"
+
+alias ls="exa --icons --color=always"
+alias la="exa -alhGF --sort=ext --color=always --icons"
